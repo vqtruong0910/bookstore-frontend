912fb78bb80ac129ca2794ee21faadb585c87cfb
Quang Truong <votruong284@gmail.com>
2022-09-17 20:36:35 +0700

    Fix router

--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense, useCallback } from 'react';
 import { PATH } from '../constants/path';
+import { Container } from 'postcss';
 
 const DefaultLayout = lazy(() => import('../layout/DefaultLayout'))
 const Main = lazy(() => import('../page/Main'));
@@ -10,16 +11,15 @@ const Register = lazy(() => import('../page/Register'));
 const ForgotPassword = lazy(() => import('../page/Forgotpassword'));
 
 function MainRoutes() {
-    const defaultLayoutMain = useCallback(() => <DefaultLayout><Main /></DefaultLayout>, [])
-    const defaultLayoutNotFound = useCallback(() => <DefaultLayout><NotFound /></DefaultLayout>, [])
+    const defaultLayout = useCallback(Container => <DefaultLayout><Container /></DefaultLayout>, [])
     return (
         <Suspense fallback={<div>Loading...</div>}>
             <Routes>
-                <Route index path={PATH.main} element={defaultLayoutMain()}></Route>
+                <Route index path={PATH.main} element={defaultLayout(Main)}></Route>
                 <Route path={PATH.login} element={<Login />}></Route>
                 <Route path={PATH.register} element={<Register />}></Route>
                 <Route path={PATH.forgotpassword} element={<ForgotPassword />}></Route>
-                <Route path={PATH.notfound} element={defaultLayoutNotFound()}></Route>
+                <Route path={PATH.notfound} element={defaultLayout(NotFound)}></Route>
             </Routes>
         </Suspense>
     );
