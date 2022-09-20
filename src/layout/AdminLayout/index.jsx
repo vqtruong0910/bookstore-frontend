246ebd8a79dcdb406c212b61e2a067299b91c5f4
Quang Truong <votruong284@gmail.com>
2022-09-21 00:17:54 +0700

    Step 1 for UI Admin

--- src/layout/AdminLayout/index.jsx
+++ src/layout/AdminLayout/index.jsx
@@ -4,7 +4,7 @@ function AdminLayout() {
     return (
         <div className="w-full bg-slate-50 h-screen relative">
 
-            <div className="flex flex-col w-64 -translate-x-64 h-screen bg-slate-700 absolute z-40 invisible lg:translate-x-0 lg:visible transition-all ">
+            <div className="flex flex-col w-64 -translate-x-64 h-screen bg-slate-800 absolute z-40 invisible lg:translate-x-0 lg:visible transition-all ">
                 <h1 className="text-center font-lobster text-slate-50 font-bold text-4xl p-3 select-none">Book Store</h1>
                 <ul className="mt-3">
                     <li>
@@ -46,7 +46,7 @@ function AdminLayout() {
                 </div>
             </header>
             <div className="grid grid-cols-1 gap-5 pt-24 px-5 w-full md:grid-cols-2 lg:pl-72">
-                <div className="min-h-24 p-5 bg-slate-400 rounded-lg">
+                <div className="md:col-span-2 min-h-24 p-5 bg-slate-200 rounded-lg">
                     <h2>Xin chào</h2>
                     <p>Đây là những gì đang xảy ra với các dự án của bạn ngày hôm nay:</p>
                 </div>
