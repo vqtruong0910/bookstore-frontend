a7b81aa423f27dd74e884d59010a8624195076fd
VONGOCMINHTRANG <vongocminhtrang2712@gmail.com>
2022-09-27 11:02:14 +0700

    [#9] Question/Create slide show product

--- src/page/Main/index.jsx
+++ src/page/Main/index.jsx
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import Content from "../../components/Content";
 
 function Main() {
-
-
     return (
         <>
-            <h1 className="text-3xl font-bold underline">Dashboard</h1>
+            {/* <h1 className="text-3xl font-bold underline">Dashboard</h1>
             <Link to="/askdfjaslkf">Nhan vo</Link>
+             */}
+             
+            <Content />
         </>
     );
 }
