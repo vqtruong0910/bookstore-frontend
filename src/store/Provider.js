697b0c85f1183a6fa73982d36593fcee5bc9e280
Quang Truong <votruong284@gmail.com>
2022-10-14 17:28:15 +0700

    [#32] Update function login

--- src/store/Provider.js
+++ src/store/Provider.js
@@ -1,11 +1,10 @@
-import jwtDecode from "jwt-decode";
 import { useMemo, useState } from "react";
 import Context from "./Context";
 
 function Provider({ children }) {
     const defaultUser = useMemo(() => {
         try {
-            return jwtDecode(localStorage.getItem("token"));
+            return JSON.parse(localStorage.getItem("auth-user"));
         } catch (error) {
             return false;
         }
