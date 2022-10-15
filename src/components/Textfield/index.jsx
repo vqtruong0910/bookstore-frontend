6a6798c957af9a45a02c452b429ddffef6903d47
Quang Truong <votruong284@gmail.com>
2022-10-15 14:12:42 +0700

    Fix MenuAdmin and TexxtField

--- src/components/Textfield/index.jsx
+++ src/components/Textfield/index.jsx
@@ -10,6 +10,7 @@ function Textfield({
     style,
     type,
     placeholder = "Hãy viết gì đó...",
+    disabled,
     control,
     name,
     rules
@@ -17,8 +18,16 @@ function Textfield({
     const { field, fieldState } = useController({ control, name, rules });
     return (
         <div className={clsx("flex flex-col", marginY, marginX, marginT, marginB)}>
-            <input type={type} className={clsx(fieldState.error && "border-red-600", "p-3 border transition-colors focus:outline-none focus:placeholder-slate-300 block", rounded, style)} placeholder={placeholder} {...field} />
-            {fieldState.error && <span className="px-2 italic text-sm text-red-500">{fieldState.error.message}*</span>}
+            {!disabled ?
+                <>
+                    <input type={type} className={clsx(fieldState.error && "border-red-600", "p-3 border transition-colors focus:outline-none focus:placeholder-slate-300 block", rounded, style)} placeholder={placeholder} {...field} />
+                    {fieldState.error && <span className="px-2 italic text-sm text-red-500">{fieldState.error.message}*</span>}
+                </> :
+                <>
+                    <input type={type} className={clsx(fieldState.error && "border-red-600", "p-3 border transition-colors focus:outline-none focus:placeholder-slate-300 block", rounded, style)} placeholder={placeholder} {...field} disabled />
+                    {fieldState.error && <span className="px-2 italic text-sm text-red-500">{fieldState.error.message}*</span>}
+                </>
+            }
         </div>
     );
 }
--- src/page/ChangePassword/index.jsx
+++ src/page/ChangePassword/index.jsx
@@ -23,7 +23,7 @@ function ChangePassword() {
         <>
             <h3 className="text-center p-5 text-lg font-normal">Nhập lại mật khẩu mới</h3>
             <form action="" method="post" onSubmit={handleSubmit(onSubmit)}>
-                <Textfield type="text" marginX="mx-4" placeholder="Email" control={control} name="Email" rules={VALIDATE.email} />
+                <Textfield type="text" marginX="mx-4" placeholder="Email" disabled control={control} name="Email" rules={VALIDATE.email} />
                 <Textfield type="password" marginX="mx-4" marginT="mt-4" marginB="mb-2" placeholder="Mật khẩu" control={control} name="MatKhauDeXuat" rules={VALIDATE.password} />
                 <Textfield type="password" marginX="mx-4" marginT="mt-4" marginB="mb-2" placeholder="Nhập lại mật khẩu" control={control} name="MatKhau" rules={VALIDATE.password} />
                 <Button marginY="my-2" marginX="mx-4" loading={loading}>Xác nhận</Button>
